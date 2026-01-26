43db4fca831747f1f69b921c943e9208de526531
LuizinVS <luiz.mariath3@gmail.com>
2026-01-26 15:46:37 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/luiz/autocare/autocare_scheduler/AutocareSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.autocare.autocare_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocareSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocareSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/autocare/autocare_scheduler/AutocareSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.autocare.autocare_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocareSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
